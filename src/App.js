7df63d541e68acee2c0dca529c05277a0c577606
Matan Borenkraout <matan.borenkraout@nielsen.com>
2020-01-11 15:49:46 +0200

    Remove more code from unnecessary places

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import './App.css';
-import PokemonsList from "./components/PokemonsList";
-import SearchBox from './components/SearchBox';
-import {sendAnalyticsPing} from './helpers/utils';
-import Description from './components/Description';
-import Header from './components/Heder';
+import React from 'react'
+import './App.css'
+import PokemonsList from './components/PokemonsList'
+import SearchBox from './components/SearchBox'
+import { sendAnalyticsPing } from './helpers/utils'
+import Description from './components/Description'
+import Header from './components/Heder'
 
-class App extends React.Component{
-    state = {
-        inputValue: ''
-    };
+class App extends React.Component {
+  state = {
+    inputValue: ''
+  }
 
-    onInputChange = (value) => {
-        this.setState({inputValue: value});
-        sendAnalyticsPing(value);
-    };
+  onInputChange = (value) => {
+    this.setState({ inputValue: value })
+    sendAnalyticsPing(value)
+  }
 
-    render(){
-        const {pokemons, inputValue} = this.state;
+  render () {
+    const { inputValue } = this.state
 
-        return (
-            <div className="App">
-                <Header>Pokédex</Header>
-                <SearchBox inputValue={inputValue} onChange={this.onInputChange}/>
-                <PokemonsList pokemons={pokemons} searchValue={inputValue}/>
-                <Description/>
-            </div>
-        )
-    }
+    return (
+      <div className="App">
+        <Header>Pokédex</Header>
+        <SearchBox inputValue={inputValue} onChange={this.onInputChange}/>
+        <PokemonsList searchValue={inputValue}/>
+        <Description/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
